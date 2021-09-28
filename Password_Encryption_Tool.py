9a8afd4a8454e926e4f0d0cc16b9aa28c047761d
senevren <77117171+senevren@users.noreply.github.com>
2021-09-28 16:02:53 -0500

    Add files via upload

---
+++ Password_Encryption_Tool.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import argparse
+import bcrypt
+ 
+parser = argparse.ArgumentParser(description='Password Encryption Tool v1.0')
+ 
+parser.add_argument('-v', '--version', action='version', version='Password Encryption Tool v1.0')
+ 
+parser.add_argument('password', type=str, help="The password to analyze")
+ 
+args = parser.parse_args()
+ 
+password = args.password
+ 
+encoded_password = password.encode('ascii')
+ 
+encrypted_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+ 
+print(encrypted_password)
+
